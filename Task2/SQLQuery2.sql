28c556e134bdd58110e74916b42ac30613e8d3c2
Shashank Dimri(Personal) <shashankdimri28@gmail.com>
2025-07-07 15:22:57 +0530

    FINAL

--- Task2/SQLQuery2.sql
+++ Task2/SQLQuery2.sql
@@ -1,4 +1,4 @@
 INSERT INTO Employees (FirstName, LastName, Email, Salary)
 VALUES
- ('John', 'Doe', 'john.doe@company.com', 50000.00),
- ('Jane', 'Smith', 'jane.smith@company.com', 55000.00);
+ ('Shashank', 'Dimri', 'shashankdimri28@gmail.com', 1.00),
+ ('AppliedInfo', 'Services', 'shashankdimri2003@gmail.com', 2.00);
--- Task2/SQLQuery3.sql
+++ Task2/SQLQuery3.sql
@@ -1,1 +1,1 @@
-SELECT * FROM Employees WHERE Salary > 50000;
+SELECT * FROM Employees WHERE Salary > 1;
